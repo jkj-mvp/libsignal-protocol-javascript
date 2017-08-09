4c98ed138dd14c8cf3e2f8b6e978fec1895263d9
Justino Mora <jmora258@gmail.com>
2017-08-08 23:27:01 -0700

    Generate identity keys, prekeys, and signed prekeys. To do: send keys to server.

--- client.js
+++ client.js
@@ -1,60 +1,87 @@
 KeyHelper = libsignal.KeyHelper;
 
-const store = SignalProtocolStore();
+const store = new SignalProtocolStore();
 
 const registrationId = KeyHelper.generateRegistrationId();
 // Store registrationId somehwere durable and safe.
 console.log('My registrationId is: ', registrationId);
 
-KeyHelper.generateIdentityKeyPair().then(function(identityKeyPair) {
+// let longTermKeyPair = {};  --> not needed 
+
+const keyID = 1337; // Our made up key ID 
+
+// const registerID = () => {}; 
+ const keyBundle = {}; 
+
+KeyHelper.generateIdentityKeyPair().then((identityKeyPair) => {
     // keyPair -> { pubKey: ArrayBuffer, privKey: ArrayBuffer }
     // Store identityKeyPair somewhere durable and safe.
     console.log('My identityKeyPair is: ', identityKeyPair);
-    console.log('My identityKeyId is: ', identityKeyPair.id);
-    store.identityKeyPair(identityKeyPair.id)
+    console.log("Pub key ", identityKeyPair.pubKey); 
+    store.put("identityKey", identityKeyPair); 
+    // longTermKeyPair =  identityKeyPair; // store.getIdentityKeyPair(); 
+
+    KeyHelper.generatePreKey(keyID).then(function(preKey) {
+        store.storePreKey(preKey.keyId, preKey.keyPair);
+        console.log("Our prekeys are: ", preKey);
+
+        KeyHelper.generateSignedPreKey(identityKeyPair, keyID).then(function(signedPreKey) {
+            store.storeSignedPreKey(signedPreKey.keyId, signedPreKey.keyPair);
+            console.log("Our signedPreKey ", signedPreKey); 
+
+            
+            // RIGHT HERE 
+            // Retrieve: 1) identityKeyPair 2) preKeyPair 3) signedPreKeyPair  TO SEND to server 
+            // 1: store.getIdentityKeyPair(); 
+            store.getIdentityKeyPair().then( identKeyPair => {
+                console.log("For server - longTermKeys: ", identKeyPair);
+                keyBundle.identityKeyPair = identKeyPair;   
+            });
+
+            // 2: store.loadPreKey(keyID); 
+            store.loadPreKey(keyID).then( ourPreKey => {
+                console.log("For server - PreKey: ", ourPreKey); 
+                keyBundle.preKey = ourPreKey; 
+            });
+            
+
+            // 3: store.loadSignedPreKey(keyID); 
+            store.loadSignedPreKey(keyID).then( ourSignedPreKeys => {
+                console.log("For server - signedPreKey: ", ourSignedPreKeys); 
+                keyBundle.signedPreKey = ourSignedPreKeys; 
+            }); 
+        });
+    }); 
 });
 
 
-KeyHelper.generatePreKey(keyId).then(function(preKey) {
-    store.storePreKey(preKey.id, preKey.keyPair);
+// SESSION 
+var address = new libsignal.SignalProtocolAddress(recipientId, deviceId);
+
+// Instantiate a SessionBuilder for a remote recipientId + deviceId tuple.
+SessionBuilder = new libsignal.SessionBuilder(store, address);
+
+// Process a prekey fetched from the server. Returns a promise that resolves
+// once a session is created and saved in the store, or rejects if the
+// identityKey differs from a previously seen identity for this address.
+var promise = sessionBuilder.processPreKey({
+    registrationId: Number,
+    identityKey: ArrayBuffer,
+    signedPreKey: {
+        keyId     : Number,
+        publicKey : ArrayBuffer,
+        signature : ArrayBuffer
+    },
+    preKey: {
+        keyId     : Number,
+        publicKey : ArrayBuffer
+    }
 });
 
-KeyHelper.generateSignedPreKey(identityKeyPair, keyId).then(function(signedPreKey) {
-    store.storeSignedPreKey(signedPreKey.keyId, signedPreKey.keyPair);
+promise.then(function onsuccess() {
+  // encrypt messages
 });
 
-console.log('Our store should have our ID, keys: ', store);
-
-// SESSION
-
-// var store   = new MySignalProtocolStore();
-// var address = new libsignal.SignalProtocolAddress(recipientId, deviceId);
-
-// // Instantiate a SessionBuilder for a remote recipientId + deviceId tuple.
-// //sessionBuilder (?) 
-// SessionBuilder = new libsignal.SessionBuilder(store, address);
-
-// // Process a prekey fetched from the server. Returns a promise that resolves
-// // once a session is created and saved in the store, or rejects if the
-// // identityKey differs from a previously seen identity for this address.
-// var promise = sessionBuilder.processPreKey({
-//     registrationId: Number,
-//     identityKey: ArrayBuffer,
-//     signedPreKey: {
-//         keyId     : Number,
-//         publicKey : ArrayBuffer,
-//         signature : ArrayBuffer
-//     },
-//     preKey: {
-//         keyId     : Number,
-//         publicKey : ArrayBuffer
-//     }
-// });
-
-// promise.then(function onsuccess() {
-//   // encrypt messages
-// });
-
-// promise.catch(function onerror(error) {
-//   // handle identity key conflict
-// });
+promise.catch(function onerror(error) {
+  // handle identity key conflict
+});
